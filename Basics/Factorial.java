728a1ea9ba8c275c1abb9c2545d80e9a9e717dd2
Vipul1142 <vipuljha1142@gmail.com>
2020-10-11 12:52:51 +0530

    closed scanner variable

--- Basics/Factorial.java
+++ Basics/Factorial.java
@@ -13,16 +13,13 @@ public class Factorial {
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
-
     int result = 1;
 
     for (int i = 1; i <= n; i++) {
-
       result *= i;
-
     }
-
     System.out.println(result);
+    sc.close();
   }
 
 }
