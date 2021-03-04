fdb6232dfcc53de975583aa6860c96b1e6dd0fc9
vijay <vijaybalajitheboss@gmail.com>
2021-03-04 10:28:53 +0530

    Updated program to count odd and even numbers

--- Basics/count_odd_or_even.java
+++ Basics/count_odd_or_even.java
@@ -9,22 +9,27 @@ class count_odd_or_even
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number >");
         int num = sc.nextInt();
-        int even = 0, odd = 0;
+        int even = 0, odd = 0, sum_of_even = 0, sum_of_odd = 0;
         while(num>0)
         {
-            switch((num%10)%2)
+            int temp = (num%10);
+            switch(temp%2)
             {
                 case 0:
                     even++;
+                    sum_of_even = sum_of_even + temp;
                     break;
                 case 1:
                     odd++;
+                    sum_of_odd = sum_of_odd + temp;
                     break;
             }
             num = num/10;
         }
         System.out.println("Number of even numbers -> " + even);
+        System.out.println("Sum of all the even numbers -> " + sum_of_even);
         System.out.println("Number of odd numbers -> " + odd);
+        System.out.println("Sum of all the odd numbers -> " + sum_of_odd);
         sc.close();
     }
 }
