f0ca0a55c9366962ff46c0d918ccc57701f131a7
vijay <vijaybalajitheboss@gmail.com>
2021-04-18 22:05:33 +0530

    Added program for Indexed File Allocation policy

---
+++ Miscellaneous/IndexedFileAllocation.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of blocks : ");
+        int blockSize = sc.nextInt();
+        System.out.print("Enter maximum data block hold by index blocks : ");
+        int maxDataBlockSize = sc.nextInt();
+        boolean blocks[] = new boolean[blockSize];
+        System.out.print("Do you wish to allocate new file ?(Y/N) : ");
+        char ans = sc.next().charAt(0);
+        while (ans == 'Y' || ans == 'y') 
+        {
+            System.out.print("Enter index block number : ");
+            int idxBlock = sc.nextInt();
+            if (blocks[idxBlock] == false) 
+            {
+                ArrayList<Integer> arr = new ArrayList<Integer>();
+                boolean in = true;
+                for (int i = 0; i < maxDataBlockSize; i++) 
+                {
+                    System.out.print("Enter the " + (i + 1) + "data block no. : ");
+                    int bn = sc.nextInt();
+                    if (bn >= 0 && bn < blocks.length && blocks[bn] == false && bn != idxBlock)
+                    {
+                        if (arr.contains(bn)) 
+                        {
+                            in = false;
+                            break;
+                        }
+                        arr.add(bn);
+                    } 
+                    else if (bn == -1) 
+                    {
+                        continue;
+                    } 
+                    else 
+                    {
+                        in = false;
+                        break;
+                    }
+                }
+                if (in) 
+                {
+                    for (int idx : arr) 
+                    {
+                        blocks[idx] = true;
+                    }
+                    blocks[idxBlock] = true;
+                    System.out.println("File Allocated Successfully !");
+                } 
+                else 
+                {
+                    System.out.println("File Allocation Failed!");
+                }
+            } 
+            else 
+            {
+                System.out.println("File Allocation Failed !");
+            }
+            System.out.print("Do you wish to continue ? (Y/N) :");
+            ans = sc.next().charAt(0);
+        }
+    }
+}
--- Miscellaneous/bestFitVsfirstFit.java
+++ Miscellaneous/bestFitVsfirstFit.java
@@ -8,7 +8,8 @@ public class bestFitVsfirstFit {
         System.out.println("enter the number of the segments");
         int segments = n.nextInt();
         int[] segment_values = new int[segments];
-        for (int i = 0; i < segments; i++) {
+        for (int i = 0; i < segments; i++) 
+        {
             System.out.println("value of the segments" + (i + 1));
             segment_values[i] = n.nextInt();
         }
@@ -16,7 +17,8 @@ public class bestFitVsfirstFit {
         int processes = n.nextInt();
         int processes_values[] = new int[processes];
         String array_process_name[] = new String[processes];
-        for (int i = 0; i < processes; i++) {
+        for (int i = 0; i < processes; i++) 
+        {
             n.nextLine();
             System.out.println("enter the name of the process");
             array_process_name[i] = n.nextLine();
@@ -30,16 +32,19 @@ public class bestFitVsfirstFit {
 
     }
 
-    public static void first_come(int segments[], int processes[], String array_process_name[]) {
+    public static void first_come(int segments[], int processes[], String array_process_name[]) 
+    {
         String display_pattern[] = new String[array_process_name.length];
         boolean checking[] = new boolean[segments.length];
-        for (int i = 0; i < checking.length; i++) {
+        for (int i = 0; i < checking.length; i++) 
+        {
             checking[i] = true;
         }
         loop1:
         for (int j = 0; j < processes.length; j++) {
             for (int i = 0; i < segments.length; i++) {
-                if (processes[j] <= segments[i] && checking[i] == true) {
+                if (processes[j] <= segments[i] && checking[i] == true) 
+                {
                     checking[i] = false;
                     display_pattern[j] = Integer.toString(i + 1);
                     continue loop1;
@@ -52,33 +57,41 @@ public class bestFitVsfirstFit {
 
     }
 
-    public static void best_fit(int segments[], int processes[], String array_process_name[]) {
+    public static void best_fit(int segments[], int processes[], String array_process_name[]) 
+    {
         String display_pattern[] = new String[array_process_name.length];
         boolean checking[] = new boolean[segments.length];
         int minimum[] = new int[segments.length];
         int reference = 0;
         int position = 0;
 
-        for (int i = 0; i < checking.length; i++) {
+        for (int i = 0; i < checking.length; i++) 
+        {
             checking[i] = true;
         }
         loop1:
-        for (int j = 0; j < processes.length; j++) {
-            for (int k = 0; k < minimum.length; k++) {
+        for (int j = 0; j < processes.length; j++) 
+        {
+            for (int k = 0; k < minimum.length; k++) 
+            {
                 minimum[k] = 0;
             }
 
-            for (int i = 0; i < segments.length; i++) {
-                if (processes[j] <= segments[i] && checking[i] == true) {
+            for (int i = 0; i < segments.length; i++) 
+            {
+                if (processes[j] <= segments[i] && checking[i] == true) 
+                {
                     minimum[i] = segments[i];
                     reference = segments[i];
                     position = i;
                 }
                 loop2:
-                for (int h = 0; h < minimum.length; h++) {
+                for (int h = 0; h < minimum.length; h++) 
+                {
                     if (minimum[h] == 0) {
                         continue loop2;
-                    } else if (minimum[h] < reference) {
+                    } else if (minimum[h] < reference) 
+                    {
                         reference = minimum[h];
                         position = h;
                     }
@@ -92,9 +105,11 @@ public class bestFitVsfirstFit {
 
     }
 
-    public static void printing(String array_processes_name[], String display_pattern[]) {
+    public static void printing(String array_processes_name[], String display_pattern[])
+    {
         System.out.println("process name       segment of the memory");
-        for (int i = 0; i < array_processes_name.length; i++) {
+        for (int i = 0; i < array_processes_name.length; i++) 
+        {
             System.out.println(array_processes_name[i] + "               " + display_pattern[i]);
         }
     }
